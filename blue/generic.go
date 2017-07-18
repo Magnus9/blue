55a16ae8a1983315a285e9642557030677f7a1b4
Magnus9 <Magnus9@hotmail.com>
2017-07-18 04:26:15 +0200

    forgot to add a '\n' in the tracefunc

--- blue/generic.go
+++ blue/generic.go
@@ -11,11 +11,10 @@ import (
 func genericTraceFunc(frame *objects.BlFrame) {
     var buf bytes.Buffer
     for f := frame; f != nil; f = f.Prev {
-        str := fmt.Sprintf("in %s:%d\n   %s", f.Pathname,
+        str := fmt.Sprintf("in %s:%d\n   %s\n", f.Pathname,
                            f.Node.LineNum, f.Node.Line)
         buf.WriteString(str)
     }
-    buf.WriteByte('\n')
     buf.WriteString(errpkg.Errmsg)
 
     panic(buf.String())
