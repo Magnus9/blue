f606a05375ba6ef39a2aa08c6bcabca6251fbbfb
Magnus9 <Magnus9@hotmail.com>
2017-07-30 19:44:57 +0200

    New text editor. Replaced tabs with whitespace

--- blue/builtins.go
+++ blue/builtins.go
@@ -1,40 +1,40 @@
 package blue
 
 import (
-	"github.com/Magnus9/blue/errpkg"
-	"github.com/Magnus9/blue/objects"
+    "github.com/Magnus9/blue/errpkg"
+    "github.com/Magnus9/blue/objects"
 )
 var builtins map[string]objects.BlObject
 
 func blInitBuiltins() {
-	mod := blInitModule("builtins", blBuiltinMethods)
-	mod.Locals["string"] = &objects.BlStringType
-	mod.Locals["float" ] = &objects.BlFloatType
-	mod.Locals["list"  ] = &objects.BlListType
-	mod.Locals["file"  ] = &objects.BlFileType
-	mod.Locals["bool"  ] = &objects.BlBoolType
-	mod.Locals["int"   ] = &objects.BlIntType
-	mod.Locals["socket"] = &objects.BlSocketType
-	builtins = mod.Locals
+    mod := blInitModule("builtins", blBuiltinMethods)
+    mod.Locals["string"] = &objects.BlStringType
+    mod.Locals["float" ] = &objects.BlFloatType
+    mod.Locals["list"  ] = &objects.BlListType
+    mod.Locals["file"  ] = &objects.BlFileType
+    mod.Locals["bool"  ] = &objects.BlBoolType
+    mod.Locals["int"   ] = &objects.BlIntType
+    mod.Locals["socket"] = &objects.BlSocketType
+    builtins = mod.Locals
 }
 
 func builtinLen(obj objects.BlObject,
-				args ...objects.BlObject) objects.BlObject {
-	var arg objects.BlObject
-	if objects.BlParseArguments("o", args, &arg) == -1 {
-		return nil
-	}
-	typeobj := arg.BlType()
-	if seq := typeobj.Sequence; seq != nil {
-		if seq.SeqSize != nil {
-			return objects.NewBlInt(int64(seq.SeqSize(arg)))
-		}
-	}
-	errpkg.SetErrmsg("'%s' object is not a sequence",
-					 typeobj.Name)
-	return nil
+                args ...objects.BlObject) objects.BlObject {
+    var arg objects.BlObject
+    if objects.BlParseArguments("o", args, &arg) == -1 {
+        return nil
+    }
+    typeobj := arg.BlType()
+    if seq := typeobj.Sequence; seq != nil {
+        if seq.SeqSize != nil {
+            return objects.NewBlInt(int64(seq.SeqSize(arg)))
+        }
+    }
+    errpkg.SetErrmsg("'%s' object is not a sequence",
+                     typeobj.Name)
+    return nil
 }
 
 var blBuiltinMethods = []objects.BlGFunctionObject{
-	objects.NewBlGFunction("len", builtinLen, objects.GFUNC_VARARGS),
+    objects.NewBlGFunction("len", builtinLen, objects.GFUNC_VARARGS),
 }
--- blue/sysmod.go
+++ blue/sysmod.go
@@ -9,54 +9,54 @@
 package blue
 
 import (
-	"os"
-	"path/filepath"
-	"github.com/Magnus9/blue/objects"
+    "os"
+    "path/filepath"
+    "github.com/Magnus9/blue/objects"
 )
 var (
-	BLPATH_LIST = []string{
-		"." + string(filepath.Separator) + "modules",
-		".",
-	}
+    BLPATH_LIST = []string{
+        "." + string(filepath.Separator) + "modules",
+        ".",
+    }
 )
 
 var blSystemMethods = []objects.BlGFunctionObject{
-	objects.NewBlGFunction("exit", systemExit,
-						   objects.GFUNC_VARARGS),
+    objects.NewBlGFunction("exit", systemExit,
+                           objects.GFUNC_VARARGS),
 }
 
 func systemExit(obj objects.BlObject,
-				args ...objects.BlObject) objects.BlObject {
-	var exitCode int64
-	if objects.BlParseArguments("i", args, &exitCode) == -1 {
-		return nil
-	}
-	os.Exit(int(exitCode))
-	return nil
+                args ...objects.BlObject) objects.BlObject {
+    var exitCode int64
+    if objects.BlParseArguments("i", args, &exitCode) == -1 {
+        return nil
+    }
+    os.Exit(int(exitCode))
+    return nil
 }
 
 func makePathObject(paths []string) objects.BlObject {
-	lobj := objects.NewBlList(0)
-	for _, s := range paths {
-		lobj.Append(objects.NewBlString(s))
-	}
-	return lobj
+    lobj := objects.NewBlList(0)
+    for _, s := range paths {
+        lobj.Append(objects.NewBlString(s))
+    }
+    return lobj
 }
 
 func makeArgvObject(argv []string) objects.BlObject {
-	lobj := objects.NewBlList(0)
-	for _, s := range argv {
-		lobj.Append(objects.NewBlString(s))
-	}
-	return lobj
+    lobj := objects.NewBlList(0)
+    for _, s := range argv {
+        lobj.Append(objects.NewBlString(s))
+    }
+    return lobj
 }
 
 func blInitSystem(argv []string) objects.BlObject {
-	mod := blInitModule("system", blSystemMethods)
-	mod.Locals["stdin" ] = objects.NewBlFile(os.Stdin, "r")
-	mod.Locals["stdout"] = objects.NewBlFile(os.Stdout, "w")
-	mod.Locals["stderr"] = objects.NewBlFile(os.Stderr, "w")
-	mod.Locals["path"  ] = makePathObject(BLPATH_LIST)
-	mod.Locals["argv"  ] = makeArgvObject(argv)
-	return mod
+    mod := blInitModule("system", blSystemMethods)
+    mod.Locals["stdin" ] = objects.NewBlFile(os.Stdin, "r")
+    mod.Locals["stdout"] = objects.NewBlFile(os.Stdout, "w")
+    mod.Locals["stderr"] = objects.NewBlFile(os.Stderr, "w")
+    mod.Locals["path"  ] = makePathObject(BLPATH_LIST)
+    mod.Locals["argv"  ] = makeArgvObject(argv)
+    return mod
 }
