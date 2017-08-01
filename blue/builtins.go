9b98492e59716c17abd74180f6e513403af6a593
Magnus9 <Magnus9@hotmail.com>
2017-08-01 14:27:59 +0200

    added func err to provide a stack trace

--- blue/builtins.go
+++ blue/builtins.go
@@ -6,16 +6,9 @@ import (
 )
 var builtins map[string]objects.BlObject
 
-func blInitBuiltins() {
-    mod := blInitModule("builtins", blBuiltinMethods)
-    mod.Locals["string"] = &objects.BlStringType
-    mod.Locals["float" ] = &objects.BlFloatType
-    mod.Locals["list"  ] = &objects.BlListType
-    mod.Locals["file"  ] = &objects.BlFileType
-    mod.Locals["bool"  ] = &objects.BlBoolType
-    mod.Locals["int"   ] = &objects.BlIntType
-    mod.Locals["socket"] = &objects.BlSocketType
-    builtins = mod.Locals
+var blBuiltinMethods = []objects.BlGFunctionObject{
+    objects.NewBlGFunction("len", builtinLen, objects.GFUNC_VARARGS),
+    objects.NewBlGFunction("err", builtinErr, objects.GFUNC_VARARGS),
 }
 
 func builtinLen(obj objects.BlObject,
@@ -35,6 +28,29 @@ func builtinLen(obj objects.BlObject,
     return nil
 }
 
-var blBuiltinMethods = []objects.BlGFunctionObject{
-    objects.NewBlGFunction("len", builtinLen, objects.GFUNC_VARARGS),
+/*
+ * Since there is no exception handling at the
+ * moment, this subroutine can be used to error
+ * the interpreter and give a stack-trace.
+ */
+func builtinErr(obj objects.BlObject,
+                args ...objects.BlObject) objects.BlObject {
+    var errmsg string
+    if objects.BlParseArguments("s", args, &errmsg) == -1 {
+        return nil
+    }
+    errpkg.SetErrmsg(errmsg)
+    return nil
+}
+
+func blInitBuiltins() {
+    mod := blInitModule("builtins", blBuiltinMethods)
+    mod.Locals["string"] = &objects.BlStringType
+    mod.Locals["float" ] = &objects.BlFloatType
+    mod.Locals["list"  ] = &objects.BlListType
+    mod.Locals["file"  ] = &objects.BlFileType
+    mod.Locals["bool"  ] = &objects.BlBoolType
+    mod.Locals["int"   ] = &objects.BlIntType
+    mod.Locals["socket"] = &objects.BlSocketType
+    builtins = mod.Locals
 }
