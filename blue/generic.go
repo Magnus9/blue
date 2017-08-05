7b5f51bfc8069236ee37506a433f4229b0107e08
Magnus9 <Magnus9@hotmail.com>
2017-08-05 22:23:43 +0200

    updated genericTraceFunc to provide a better stack-trace

--- blue/generic.go
+++ blue/generic.go
@@ -11,11 +11,12 @@ import (
 func genericTraceFunc(frame *objects.BlFrame) {
     var buf bytes.Buffer
     for f := frame; f != nil; f = f.Prev {
-        str := fmt.Sprintf("in %s:%d\n   %s\n", f.Pathname,
-                           f.Node.LineNum, f.Node.Line)
+        str := fmt.Sprintf("in file <%s:%d>, func %s\n   %s\n",
+                           f.Pathname, f.Node.LineNum,
+                           f.Name, f.Node.Line)
         buf.WriteString(str)
     }
     buf.WriteString(errpkg.Errmsg)
-
+    
     panic(buf.String())
 }
